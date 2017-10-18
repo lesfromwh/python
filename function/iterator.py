39aa57e7456a8f4ea6a01fc76bde66cb789e9bdf
liushuang <479825880@qq.com>
2017-10-18 17:52:26 +0800

    迭代器

---
+++ function/iterator.py
@@ -0,0 +1,14 @@
+# 迭代器
+from collections import Iterator
+
+g = (x for x in range(1, 6))
+L = [x for x in range(1, 6)]
+
+# 生成器都是Iterator对象，但list、dict、str虽然是Iterable，却不是Iterator。
+# 这是因为Python的Iterator对象表示的是一个数据流，Iterator对象可以被next()函数调用并不断返回下一个数据，
+# 直到没有数据时抛出StopIteration错误。可以把这个数据流看做是一个有序序列，但我们却不能提前知道序列的长度，只
+# 能不断通过next()函数实现按需计算下一个数据，所以Iterator的计算是惰性的，只有在需要返回下一个数据时它才会计算。
+# Iterator甚至可以表示一个无限大的数据流，例如全体自然数。而使用list是永远不可能存储全体自然数的。
+print(isinstance(g, Iterator))  # true
+print(isinstance(L, Iterator))  # false
+print(isinstance(iter(L), Iterator))  # true
