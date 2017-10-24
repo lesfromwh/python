e85228746ff912906eca3bca64a8b0ec09dd1287
liushuang <479825880@qq.com>
2017-10-24 14:54:19 +0800

    乘法

---
+++ py2/chapter2/py2.2.4_multiplication.py
@@ -0,0 +1,9 @@
+# 序列 乘法
+str = 'python'
+print(str * 2)  # pythonpython
+
+numbers = [1, 2]
+print(numbers * 2)  # [1, 2, 1, 2]
+
+sequence = [None] * 10  # 初始化长度为10的列表
+print(sequence) # [None, None, None, None, None, None, None, None, None, None]
