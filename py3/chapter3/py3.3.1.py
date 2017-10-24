8e33e86a1ba94725304dc7353788ee652b0cb3ed
liushuang <479825880@qq.com>
2017-10-24 17:10:31 +0800

    简单转化

---
+++ py3/chapter3/py3.3.1.py
@@ -0,0 +1,9 @@
+# 简单转换
+print('eggs : $%d' % 42)  # print('eggs : $%d' %42)
+
+from math import pi
+
+# f  float, i int
+print('ss %f' % pi)  # ss 3.141593
+print('ss %i' % pi)  # ss 3
+print('ss %s' % 42)  # ss 42
