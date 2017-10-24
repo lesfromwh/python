54366d9b5a32904b83a96b365627e0ce2d6c2de4
liushuang <479825880@qq.com>
2017-10-24 16:45:11 +0800

    string:join

---
+++ py3/chapter4/py3.4.2_join.py
@@ -0,0 +1,8 @@
+# join
+seq = ['1','2','3','4']
+x = '+'
+join = x.join(seq)
+print(join) # 1+2+3+4
+
+seq2 =[1,2,3,4] # 被连接的序列元素必须是字符串.
+# print(x.join(seq2)) # sequence item 0: expected str instance, int found
