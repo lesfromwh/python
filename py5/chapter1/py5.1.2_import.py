e2d43b4ce4ca681122ba2c9845ca2cc461a5a7f0
ls <479825880@qq.com>
2017-10-26 00:51:13 +0800

    import

---
+++ py5/chapter1/py5.1.2_import.py
@@ -0,0 +1,17 @@
+# import
+
+# 1.import somemodule
+# import math
+# print(math.sqrt(4)) # 2.0
+
+# 2.from somemodule import somefunction
+# from math import sqrt
+# print(sqrt(4))
+
+#还可以取别名
+# from math import sqrt as a
+# print(a(4))
+
+# 3.from somemodule import somefunction1,somefunction2,somefunction3
+
+# 4.from somemodule import*
