9d013300dfed1898538e6070c93ac984014f6a4d
liushuang <479825880@qq.com>
2017-10-26 17:59:21 +0800

    boolean

---
+++ py5/chapter4/py5.4.1_bool.py
@@ -0,0 +1,10 @@
+# 布尔
+print(bool(1))  # true
+print(bool(0))  # False
+print(bool(()))  # False
+print(bool({}))  # False
+print(bool([]))  # False
+print(bool(""))  # False
+print(bool(None))  # False
+print(bool(''))  # False
+#其他都是ture
